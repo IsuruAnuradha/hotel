3377193bc7bc9d0f9d884b1a8b41074cd582cd9a
Isuru Anuradha <Isuruanuradha99@gmail.com>
2018-03-17 11:33:54 +0800

    Reservation model added

---
+++ hotel/src/main/java/com/media/hotel/model/Reservation.java
@@ -0,0 +1,69 @@
+package com.media.hotel.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RESERVATION")
+public class Reservation {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "reservation_id")
+	private Integer id;
+	@Column(name = "booking_reference")
+	private String bookingReference;
+	@Column(name = "checkin")
+	private LocalDate chechin;
+	@Column(name = "checkout")
+	private LocalDate checkout;
+	@Column(name = "room_number")
+	private Integer roomnumber;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getBookingReference() {
+		return bookingReference;
+	}
+
+	public void setBookingReference(String bookingReference) {
+		this.bookingReference = bookingReference;
+	}
+
+	public LocalDate getChechin() {
+		return chechin;
+	}
+
+	public void setChechin(LocalDate chechin) {
+		this.chechin = chechin;
+	}
+
+	public LocalDate getCheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(LocalDate checkout) {
+		this.checkout = checkout;
+	}
+
+	public Integer getRoomnumber() {
+		return roomnumber;
+	}
+
+	public void setRoomnumber(Integer roomnumber) {
+		this.roomnumber = roomnumber;
+	}
+
+}
