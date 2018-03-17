18b852bf3fe6f5b1e3ad335af4453b27c1a7a947
Isuru Anuradha <Isuruanuradha99@gmail.com>
2018-03-17 08:09:01 +0800

    Empty project created

---
+++ hotel/src/main/java/com/media/hotel/model/RoomMaster.java
@@ -0,0 +1,74 @@
+package com.media.hotel.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROOM_MASTER")
+public class RoomMaster implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "room_master_id")
+	private Integer id;
+	@Column(name = "type")
+	private String type;
+	@Column(name = "description")
+	private String description;
+	@Column(name = "image")
+	private String image;
+	@Column(name = "price")
+	private Float price;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public Float getPrice() {
+		return price;
+	}
+
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+
+}
---
+++ hotel/src/main/java/com/media/hotel/repository/RoomMasterRepository.java
@@ -0,0 +1,9 @@
+package com.media.hotel.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.media.hotel.model.RoomMaster;
+
+public interface RoomMasterRepository extends JpaRepository<RoomMaster, Integer> {
+
+}
